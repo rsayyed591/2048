3746c82ca74f72821942be5b321d0a4c34e6b3b5
rsayyed591 <rehansayyed591@gmail.com>
2025-02-05 23:58:17 +0530

    updated link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <footer className="w-full py-4 text-center mt-auto">
         <p className="text-indigo-200">Made By Rehan</p>
         <a
-          href="https://github.com/rsayyed591/memory-game"
+          href="https://github.com/rsayyed591/2048"
           target="_blank"
           rel="noopener noreferrer"
           className="inline-flex items-center justify-center gap-2 text-red-500 hover:text-red-400 transition-colors"
